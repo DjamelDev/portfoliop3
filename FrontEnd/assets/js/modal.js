8457340f865c800f03d026bca8de33abe7730281
Djamel <djameldev@gmail.com>
2023-03-21 16:01:15 +0100

    open/close modal

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,45 +1,22 @@
-// let modal = null;
+let openModal = document.getElementById('modify-three');
+let closeModal = document.getElementById('js-modal-close');
 
-// const openModal = function(e) {
-//     e.preventDefault();
-//     const target = document.querySelector(e.target.getAttribute('href'));
-//     target.style.display = null;
-//     target.removeAttribute('aria-hidden')
-//     modal = target
-//     modal.addEventListener('click', closeModal)
-//     modal.querySelector('.btn-close').addEventListener('click', closeModal)
-// }
+openModal.addEventListener('click', fncOpenModal);
+closeModal.addEventListener('click', fncCloseModal);
 
-// const closeModal = function (e) {
-//     if (modal === null) return 
-//     e.preventDefault();
-//     modal.style.display = 'none';
-//     modal.setAttribute('aria-hidden', 'true');
-//     modal = target
-//     modal.removeEventListener('click', closeModal)
-//     modal = null;
-// }
+// Ouvre la fenêtre
+function fncCloseModal() {
+  document.getElementById('app-modal');
+  document.getElementById('modal').classList.remove('open');
+}
 
-// document.querySelectorAll('.modal-js').forEach(a => {
-//     a.addEventListener('click', openModal);
-// })
+// Ferme la fenêtre
+function fncOpenModal() {
+  document.getElementById('app-modal');
+	document.getElementById('modal').classList.add('open');
+}
 
+const galleryGrid = document.querySelector(".gallery");
+const worksApi = "http://localhost:5678/api/works";
 
-let buttonSpan = document.querySelector('span.modal-js');
-let modal = document.querySelector('.modal');
-let closeModal = document.querySelector('.btn-close');
-
-
-buttonSpan.addEventListener("click", function(){
-  modal.style.display = "flex";
-  modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
-});
-
-closeModal.addEventListener("click", function() {
-  if (modal === null ) return modal;
-  modal.style.display = "none";
-  modal.setAttribute("aria-hidden", "true");
-  modal.querySelector('.btn-close').removeEventListener('click', closeModal);
-  modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
-  modal = null;
-})
+//=======================================================================
